d1c485a1d7e52e622e084cf4e6493165612b2405
neutrogo <erbal98@gmail.com>
2023-01-11 22:49:37 +0300

    two operand addition only working

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let displayValue = '';
+let currentUseOperator = '';
+let num1 = '';
 
 function add(num1, num2) {
     return num1 + num2;
@@ -33,12 +35,43 @@ function operate(operator, num1, num2) {
 
 let numPad = document.querySelector('.numbers');
 let numButtons = Array.from(numPad.children);
+let opPad = document.querySelector('.operators');
+let opButtons = Array.from(opPad.children);
 
 numButtons.forEach((button) => (button.addEventListener('click', diplayAdd)));
+opButtons.forEach((button) => (button.addEventListener('click', setOperator)));
+
 
 //adds numbers clicked to the display
 function diplayAdd(e) {
-    displayValue = e.currentTarget.innerText;
+    let tmp = e.currentTarget.innerText;
+    displayValue = displayValue + tmp;
     let display = document.querySelector('.display');
-    display.innerText = display.innerText + displayValue;
+    display.innerText = displayValue;
 }
+
+// this currently only accepts two values, could be extended
+function setOperator(e) {
+    let operator = e.currentTarget.innerText;
+    let display = document.querySelector('.display');
+    
+    // check for empty values (ie no numbers pressed before operator)
+    if(operator !== '=') {
+        display.innerText = operator;
+        currentUseOperator = operator;
+        num1 = displayValue;
+        displayValue = '';
+    }
+    // change so the user can also input "3 =", returning "3"
+    if(operator === '=') {
+        if(num1 !== '') {
+            let result = operate(currentUseOperator, +num1, +displayValue);
+            displayValue = result;
+            display.innerText = displayValue;
+        }
+        else {
+            display.innerText = '';
+        }
+    }
+}
+
